a77f9b0b55865e36ad4a154de3ce931d076a20c1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-19 00:29:40 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'nvm'
+maintainer       'David Saenz Tagarro'
+maintainer_email 'david.saenz.tagarro@gmail.com'
+license          'MIT'
+description      'Installs/Configures nvm'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
